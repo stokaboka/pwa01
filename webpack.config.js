78788a22472614a6cd1a13313828ef0db12fb705
igorkhorev <igorhorev@gmail.com>
2018-08-22 16:41:50 +0300

    refactor: move files to src, public dirs

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,31 @@
 /*
 Copyright
 */
-module.exports = {
 
+let path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    
+    mode: 'development',
+    entry: './src/app.js',
+    devtool: 'inline-source-map',
+    
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+    
     devServer: {
-        contentBase: './'
-    }
+        contentBase: './public'
+    },
+    
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Development'
+        })
+    ],
 
 };
