2b476584b16b2af905d7ccaabcd834950a34226d
igorkhorev <igorhorev@gmail.com>
2018-10-09 16:35:48 +0300

    modify app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
 import './style.css';
+// загружаем модуль с методами регистрации service worker
 import { registerServiceWorker } from './reg_sw'
+// методы загрузки данных
 import { loadMoreEntries } from "./api";
 
+// имя файла service worker
 let serviceWorkerName = '/service-worker.js';
 
+// проверяем возможность обозревателем использовать service worker
 if ('serviceWorker' in navigator) {
+    // ловим событие "load" - окончание загрузки всех компонентов
     window.addEventListener('load', async () => {
+        // регистрируем service worker
         await registerServiceWorker(serviceWorkerName);
+        // загружаем данные для работы PWA приложения
         loadMoreEntries();
     });
 }
 
+// ловим событие "beforeinstallprompt" - предложение установить
+// PWA приложение в системе (при возможности)
+// установка возможна только при загрузке по HTTPS
+// с сертификатом web-сервера
 window.addEventListener('beforeinstallprompt', e => {
     e.preventDefault();
     e.prompt();
