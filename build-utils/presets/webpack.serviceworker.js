705c52006b22b938ae8e1df61b41f27dcc79eaf3
igorkhorev <igorhorev@gmail.com>
2018-10-18 16:44:20 +0300

    modify webpack.serviceworker.js - comments

--- build-utils/presets/webpack.serviceworker.js
+++ build-utils/presets/webpack.serviceworker.js
@@ -4,44 +4,98 @@ const {GenerateSW} = require('workbox-webpack-plugin');
 module.exports = () => ({
     plugins: [
         new GenerateSW({
-            // these options encourage the ServiceWorkers to get in there fast
-            // and not allow any straggling "old" SWs to hang around
+            
+            // если URL заканчивается / (должен загрузиться directory index)
+            // и запрос возвращает ошибку, то будет добавлено
+            // значение параметра directoryIndex к URL
+            // для поиска в кеше
+            // по умолчанию index.html
             directoryIndex: 'index.html',
+    
+            // service worker при первой загрузке не сможет обрабатывать fetch события и управлять страницей
+            // пока не завершаться стадии установки (событие install) и активации (событие  active).
+            // кроме того, события fetch не будут обрабатываться пока через service worker не пройдет загрузка страницы
+            // это значит, что страницу нужно обновить (F5)
+            // это поведение является поведением по умолчанию
+            // чтобы избежать необходимости делать перезагрузку страницы после первой загрузки
+            // мы можем установить значение clientsClaim: true
+            // в этом случае клиент берется под контроль сразу после фазы активации (событие  active)
             clientsClaim: true,
+            
+            // если service worker обслуживает несколько страниц / приложений
+            // он должен:
+            // - ожидать готовности всех клиентов - skipWaiting: false
+            // - не ждать других клиентов - skipWaiting: true
+            // имеет смысл, если clientsClaim: true
             skipWaiting: true,
-            // exclude: [/\.jpg$/, /\.png$/],
+    
+            // запретить кеширование для файлов отвечающих следующим критериям
+            // только пример
+            // кешировать картинки можно и нужно!
+            exclude: [/\.jpg$/, /\.png$/],
+            
+            // включить кеширование для файлов отвечающих следующим критериям
             include: [/\.html$/, /\.js$/, /\.css$/, /\.json$/],
+            
+            // правила действующие во время выполнения приложения
+            // обработка запросов API и т.п.
             runtimeCaching: [
                 {
-                    // Match any same-origin request that contains 'api'.
+                    // строка или регулярное выражение для отбора запросов
+                    // подходящих для данного кеша
                     urlPattern: /\.(?:html|js|css|json)$/,
-                    // Apply a network-first strategy.
-                    // handler: 'networkFirst',
+                    
+                    // стратегия обработки запросов, сохранения и выборки из кеша
+                    // варианты:
+                    // - cacheFirst: всегда обращение в кеш, если в кеше нет, то попытка загрузить из сети. некритичные для приложения данные
+                    // - cacheOnly: запрос только из кеша, данные должны бать предварительно загружены в кеш. используется редко
+                    // - networkFirst: запросы всегда в первую очередь отправляются в сеть и только в случае ошибки выбираются из кеша. подходит для часто меняющихся, динамических данных
+                    // - networkOnly: запросы всегда отправляются тоько в сеть. в кеше ничего не сохраняется
+                    // - staleWhileRevalidate: позволяет отвечать на запросы как можно быстрее: из кеша, если запрос кеширован, из сети если в кеше нет запроса. данные полученные из сети всегда обновляют кеш
+                    // подробнее: https://developers.google.com/web/tools/workbox/modules/workbox-strategies
                     handler: 'staleWhileRevalidate',
+                    
                     options: {
-                        // Use a custom cache name for this route.
+                        networkTimeoutSeconds: 10,
+                        
+                        // имя кеша
                         cacheName: 'code',
-                        // Configure custom cache expiration.
+                        
+                        // срок жизни кеша
                         expiration: {
+                            // лимит кешированных записей, если достугнут лимит будут удаляться старые записи
                             maxEntries: 5,
-                            // maxAgeSeconds: 60,
+                            // время жизни кеша, после которого кеш нужно обновлять
                             maxAgeSeconds: 86400,
                         },
-                        // Configure which responses are considered cacheable.
+                        
+                        // на основании каких факторов можно принять решение о помещении полученных из сети данных в кеш
+                        // если условие выполнено, данные можно сохранить в кеш
+                        // значением по умолчанию является:
+                        // - стратегия staleWhileRevalidate: statuses: [0, 200]
+                        // - стратегия cacheFirst: statuses: [200]
+                        // - headers: по умолчанию не проверяется
+                        // в данном случае проверяется оба условия:
                         cacheableResponse: {
+                            // допустимые коды статусы ответа сервера
                             statuses: [0, 200],
+                            // допустимые заголовки ответа сервера
                             headers: {'x-test': 'true'},
                         },
-                        // Configure the broadcast cache update plugin.
+                        // запросы, хранящиеся в кеше могут устаревать и тогда workbox принимает решение
+                        // обновить эти устаревшие записи выполнив запрос в
+                        // сеть вместо извлечения данных из кеша
+                        // это используется стратегией staleWhileRevalidate
+                        // - на шаге "revalidate" проверяются заголовки ответа из сети
+                        // и если заголовки отличаются от заголовков сохраненного в кеше запроса
+                        // по умолчанию проверяются заголовки: Content-Length, ETag, и Last-Modified
+                        // тело ответа не проверяется для улучшения производительности
+                        // при помощи BroadcastChannel API ( https://developers.google.com/web/updates/2016/09/broadcastchannel )
+                        // делается оповещение клиента об изменившися данных
                         broadcastUpdate: {
                             channelName: 'orangem-update-channel',
-                        },
-                        // Add in any additional plugin logic you need.
-                        plugins: [
-                            {
-                                cacheDidUpdate: () => /* custom plugin code */ console.log('code: cacheDidUpdate')
-                            }
-                        ],
+                        }
+                        
                     },
                 },
                 {
@@ -60,50 +114,29 @@ module.exports = () => ({
                         },
                         broadcastUpdate: {
                             channelName: 'orangem-data-channel',
-                        },
-                        plugins: [
-                            {
-                                cacheDidUpdate: () => /* custom plugin code */ console.log('data: cacheDidUpdate')
-                            }
-                        ],
+                        }
                     },
                 },
                 {
-                    // Match any same-origin request that contains 'api'.
                     urlPattern: /'api'/,
-                    // Apply a network-first strategy.
                     handler: 'networkFirst',
                     options: {
-                        // Fall back to the cache after 10 seconds.
                         networkTimeoutSeconds: 10,
-                        // Use a custom cache name for this route.
                         cacheName: 'api',
-                        // Configure custom cache expiration.
                         expiration: {
                             maxEntries: 5,
                             maxAgeSeconds: 60,
                         },
-                        // Configure which responses are considered cacheable.
                         cacheableResponse: {
                             statuses: [0, 200],
                             headers: {'x-test': 'true'},
                         },
-                        // Configure the broadcast cache update plugin.
                         broadcastUpdate: {
                             channelName: 'orangem-api-channel',
-                        },
-                        // Add in any additional plugin logic you need.
-                        plugins: [
-                            {
-                                cacheDidUpdate: () => /* custom plugin code */ console.log('api: cacheDidUpdate')
-                            }
-                        ],
+                        }
                     },
                 },
                 {
-                    // To match cross-origin requests, use a RegExp that matches
-                    // the start of the origin:
-                    // urlPattern: new RegExp('^https://cors\.example\.com/'),
                     urlPattern: new RegExp('^https://jsonplaceholder\.typicode\.com'),
                     // handler: 'staleWhileRevalidate',
                     handler: 'networkFirst',
